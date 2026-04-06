704f04207ba819af8125bed8a38f72f3c77717af
Ming <aistore@AIs-MacBook-Pro.local>
2026-04-06 22:02:03 +0700

    add provider for main

---
+++ lib/main_dev.dart
@@ -0,0 +1,25 @@
+import 'package:provider/provider.dart';
+import 'package:velo_toulose/main_common.dart';
+
+List<InheritedProvider> get devProviders {
+  // final appSettingsRepository = AppSettingsRepositoryMock();
+  // final songRepository = SongRepositoryFirebase();
+  return [
+    // // 1 - Inject repositories
+    // Provider<SongRepository>(create: (_) => songRepository),
+    // Provider<ArtistRepository>(create: (_) => ArtistRepositoryFirebase(
+    //     songRepository: songRepository,
+    //   )),
+
+    // // 2 - Inject the player state
+    // ChangeNotifierProvider<PlayerState>(create: (_) => PlayerState()),
+
+    // // 3 - Inject the  app setting state
+    // ChangeNotifierProvider<AppSettingsState>(
+    //   create: (_) => AppSettingsState(repository: appSettingsRepository),
+  ];
+}
+
+void main() {
+  mainCommon(devProviders);
+}
