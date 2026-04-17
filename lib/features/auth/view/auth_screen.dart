0de85aeb6b230124743e9e66f9326ef57f7cf9b9
Ming <aistore@AIs-MacBook-Pro.local>
2026-04-17 20:16:12 +0700

    auth screen

--- lib/features/auth/view/auth_screen.dart
+++ lib/features/auth/view/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:velo_toulose/core/constant/app_color.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -7,7 +8,65 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+            backgroundColor: AppColor.primary,
+      body: 
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                  'Login',style: TextStyle(
+                    color: AppColor.background,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w800
+                
+                  ),
+                
+                ),
+              
+              Text('Welcome Back',style: TextStyle(
+                color: AppColor.background,
+                fontSize: 15,
+                fontWeight: FontWeight.w500
+              ),),
+                ],
+                    ),
+            ),
+          ),
+          Expanded(
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: -100,
+                  right: 0,
+                  child: Lottie.asset('assets/stickers/girl.json'),
+                  width: 300,
+                  ),
+                Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: AppColor.background
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      
+                    )
+                  ],
+                )
+                          ),
+              ],
+            )),
 
+        ],
+      )
+      
     );
   }
 }
