dce5a354b06b54f24e7910b1acb731387464cac1
Ming <aistore@AIs-MacBook-Pro.local>
2026-04-17 19:26:57 +0700

    revert

--- lib/features/auth/view/auth_screen.dart
+++ lib/features/auth/view/auth_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:velo_toulose/core/constant/app_color.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+
+    );
   }
 }
